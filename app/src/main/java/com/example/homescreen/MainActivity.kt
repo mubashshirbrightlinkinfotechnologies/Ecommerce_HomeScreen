56ddd969df9436110b6f56039b427e5767fda918
Mubashshir <mubshshir4u@gmail.com>
2024-05-06 18:37:11 +0530

    initial stage

---
+++ app/src/main/java/com/example/homescreen/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.homescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity<Button : View?> : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
